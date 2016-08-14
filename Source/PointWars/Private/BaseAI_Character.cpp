e2b5e558726a1925d7f58debdab696d6a3e22c3c
wwalker2 <wwalker2@xula.edu>
2016-08-14 12:05:55 -0500

    Commit 9

---
+++ Source/PointWars/Private/BaseAI_Character.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PointWars.h"
+#include "BaseAI_Character.h"
+
+
+// Sets default values
+ABaseAI_Character::ABaseAI_Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABaseAI_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseAI_Character::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void ABaseAI_Character::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
